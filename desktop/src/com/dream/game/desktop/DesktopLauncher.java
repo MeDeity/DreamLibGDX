d4c3435afba949e2d2807e946f96437f3d363ca8
MeDeity <546024423@qq.com>
2017-03-30 21:00:02 +0800

    Initial commit

---
+++ desktop/src/com/dream/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dream.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dream.game.MainGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGdxGame(), config);
+	}
+}
